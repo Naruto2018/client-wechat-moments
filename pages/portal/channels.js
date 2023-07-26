a5f207ddcee65d8b8a41ac62d8ce5a0719a3e7cb
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-07-26 16:39:44 +0800

    chore: channels

--- pages/portal/channels.js
+++ pages/portal/channels.js
@@ -3,16 +3,27 @@
  * Copyright 2021-Present 唐杰
  * Licensed under the Apache-2.0 license
  */
+import { fresnsApi } from '../../api/api';
 import { fresnsConfig, fresnsLang } from '../../api/tool/function';
 
 Page({
   /** 外部 mixin 引入 **/
-  mixins: [require('../../mixins/themeChanged'), require('../../mixins/checkSiteMode')],
+  mixins: [
+    require('../../mixins/themeChanged'),
+    require('../../mixins/checkSiteMode'),
+    require('../../mixins/fresnsExtensions'),
+  ],
 
   /** 页面的初始数据 **/
   data: {
     title: null,
     fresnsConfig: null,
+
+    // 置顶帖子
+    stickyPosts: [],
+
+    // 扩展频道
+    channels: [],
   },
 
   /** 监听页面加载 **/
@@ -21,9 +32,33 @@ Page({
       title: await fresnsLang('discover'),
     });
 
+    // 置顶帖子
+    const resultRes = await fresnsApi.post.postList({
+      stickyState: 3,
+      whitelistKeys: 'pid,title,content',
+    });
+
+    let stickyPosts = [];
+    if (resultRes.code === 0) {
+      resultRes.data.list.forEach(post => {
+        post.shortContent = post.content.substring(0, 20);
+        stickyPosts.push(post);
+      });
+    }
+
+    // 频道扩展
+    const resultChannels = await fresnsApi.global.globalChannels();
+
+    let channels = [];
+    if (resultChannels.code === 0) {
+      channels = resultChannels.data;
+    }
+
     this.setData({
       title: await fresnsLang('discover'),
       fresnsConfig: await fresnsConfig(),
+      stickyPosts: stickyPosts,
+      channels: channels,
     });
   },
 
