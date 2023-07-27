bd5734dafdffff129a8554dfc988c6cb1d3bdacb
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-07-27 20:42:44 +0800

    chore: global config

--- mixins/globalConfig.js
+++ mixins/globalConfig.js
@@ -3,3 +3,19 @@
  * Copyright 2021-Present 唐杰
  * Licensed under the Apache-2.0 license
  */
+import { globalInfo } from '../utils/fresnsGlobalInfo';
+
+module.exports = {
+  /** 页面的初始数据 **/
+  data: {
+    theme: 'light',
+  },
+
+  /** 监听页面加载 **/
+  onLoad() {
+  },
+
+  /** 监听页面退出 **/
+  onUnload() {
+  },
+};
