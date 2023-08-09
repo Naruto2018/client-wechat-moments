8165b01fcd3bd18102216255067e9afc36a7456b
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-08-09 19:07:57 +0800

    build: v1.0.0

--- utils/fresnsGlobalInfo.js
+++ utils/fresnsGlobalInfo.js
@@ -8,7 +8,8 @@ import { fresnsConfig } from '../api/tool/function';
 import { fresnsLogin } from './fresnsLogin';
 
 export class GlobalInfo {
-  clientVersion = '2.1.0';
+  clientName = 'Moments';
+  clientVersion = '1.0.0';
   theme = null;
 
   // 处理用户主页路径
