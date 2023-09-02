43bd6fd3fec8848b4f0dcbf10680ea573de80d6a
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-09-02 11:30:51 +0800

    build: v1.1.2

--- utils/fresnsGlobalInfo.js
+++ utils/fresnsGlobalInfo.js
@@ -10,7 +10,7 @@ import { cacheGet, cachePut } from './fresnsUtilities';
 
 export class GlobalInfo {
   clientName = 'Moments';
-  clientVersion = '1.1.1';
+  clientVersion = '1.1.2';
   theme = null;
 
   // 处理用户主页路径
